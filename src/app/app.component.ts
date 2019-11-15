bb9ee9e91d7e87ff263d8ac0c8a278bd439631f7
ARUNKUMARTRATR <arunkumar.tr@experionglobal.com>
2019-11-15 06:35:17 -0800

    key removed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -38,7 +38,7 @@ constructor(private http: HttpClient,private snackBar: MatSnackBar){}
     else
     {
     
-    this.http.get('https://api.github.com/users/'+this.name+'?access_token=9914c840025c4510f94510aef265888294e7984a').subscribe(response =>{this.Res=response;console.log(this.Res);
+    this.http.get('https://api.github.com/users/'+this.name).subscribe(response =>{this.Res=response;console.log(this.Res);
     
     localStorage.setItem(this.name,JSON.stringify(this.Res));
     this.isLoading = false;
